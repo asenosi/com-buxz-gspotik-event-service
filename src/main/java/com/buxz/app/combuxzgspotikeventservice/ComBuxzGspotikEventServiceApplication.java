e72a54eb7aa10cdefd9739aa70ab76450bd273be
Aobakwe Senosi <senosiag@gmail.com>
2023-01-28 22:02:35 +0200

    Journey of 1000s commits

---
+++ src/main/java/com/buxz/app/combuxzgspotikeventservice/ComBuxzGspotikEventServiceApplication.java
@@ -0,0 +1,13 @@
+package com.buxz.app.combuxzgspotikeventservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ComBuxzGspotikEventServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ComBuxzGspotikEventServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/buxz/app/combuxzgspotikeventservice/ComBuxzGspotikEventServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.buxz.app.combuxzgspotikeventservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComBuxzGspotikEventServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
